0bb27b8df56a66190b5623dd3348da856892f1bf
madhuvijay95 <madhuvijay95@gmail.com>
2016-01-26 00:46:48 -0500

    Commented files_to_data.py.

--- files_to_data.py
+++ files_to_data.py
@@ -1,13 +1,29 @@
-#from year_sort import file_to_year, remove_quotes
-from test_real import year
+#######################################################################################################################
+# General goal: Take a list of file names for cases, and output a csv containing basic data (file name, case name, court
+# name, year, and docket number) for each case.
+#
+# Takes 2 inputs:
+# (1) An input file name, which contains a list of files names, each on one line.
+# (2) An output file name (csv)
+# After this runs, the output contains the following 5 fields (comma-separated), for each file name in the input file in
+# order: (1) file name, (2) abbreviated case name, (3) court name, (4) year, and (5) docket number. The docket number is
+# is left blank if not available, and fields 2 through 5 are all left blank if the file name is invalid.
+# (So, if the input file contains some lines that are file names and some lines that aren't, then the output file will
+# keep the non-file-name lines intact and will retrieve the data corresponding to each file-name line.
+#######################################################################################################################
+
+from helpers import remove_quotes
 import sys
 import xml.etree.ElementTree as ET
 import csv
 
+# Namespace used for parsing the XML files.
 ns = {'default' : 'http://www.loc.gov/METS/', 'xlink' : 'http://www.w3.org/1999/xlink',
       'casebody' : 'http://nrs.harvard.edu/urn-3:HLS.Libr.US_Case_Law.Schema.Case_Body:v1',
       'case' : 'http://nrs.harvard.edu/urn-3:HLS.Libr.US_Case_Law.Schema.Case:v1'}
 
+# Convert a file into the corresponding 5-tuple containing the data for that file.
+# Return a 1-tuple with the file name if the file is invalid.
 def file_to_data(filename):
     try:
         tree = ET.parse(filename)
@@ -24,20 +40,16 @@ def file_to_data(filename):
     except IOError:
         return (filename,)
 
-def remove_quotes(filename):
-    if len(filename) == 0:
-        return ''
-    elif filename[0] == '\'' and filename[-1] == '\'':
-        return filename[1:(len(filename)-1)]
-    else:
-        return filename
-
+# Open the input file (the 1st argument), and turn it into a list of file names.
 with open(sys.argv[1], 'rb') as input_file:
     case_list = list(input_file)
     case_list = map(lambda s : s.replace('\n','').replace('\r',''), case_list)
     case_list = map(remove_quotes, case_list)
 
+# Retrieve data for each file.
 data = map(file_to_data, case_list)
+
+# Output the data to the output file (the 2nd argument).
 with open(sys.argv[2], 'wb') as output_file:
     writer = csv.writer(output_file)
     for tup in data:
