d631ea8f57395a0c54b24c2814475abe21d6e41c
madhuvijay95 <madhuvijay95@gmail.com>
2016-01-30 14:37:20 -0500

    fixed dict errors in output_JSON_new.py -- it's now fully functional

--- output_JSON_new.py
+++ output_JSON_new.py
@@ -5,6 +5,7 @@ import sys
 from helpers import remove_quotes
 import itertools
 from collections import Counter
+import copy
 
 best_words_filename = sys.argv[1]
 clusters_filename = sys.argv[2]
@@ -42,27 +43,29 @@ cluster_data_SC_dissent = filter(lambda (f,y,t) : is_SC(f), cluster_data_dissent
 n_clusters = max(map(lambda (f,y,t) : t, cluster_data)) + 1
 years_list = map(lambda (f,y,t) : y, cluster_data)
 years = range(min(years_list), max(years_list) + 1)
-print 'done reading'
-sys.stdout.flush()
+#print 'done reading'
+#sys.stdout.flush()
 
 keys = ('cluster_id', 'cluster_words', 'case_counts', 'dissent_counts', 'SC_counts', 'SC_dissent_counts')
 baseline = dict(zip(years, map(int, np.zeros(len(years)))))
-dicts = {c : dict(zip(keys, (c, word_lists[c], baseline, baseline, baseline, baseline))) for c in range(n_clusters)}
-print 'done with baseline'
-sys.stdout.flush()
+dicts = {c : dict(zip(keys, (c, word_lists[c], copy.deepcopy(baseline), copy.deepcopy(baseline), copy.deepcopy(baseline), copy.deepcopy(baseline)))) for c in range(n_clusters)}
+#print 'done with baseline'
+#sys.stdout.flush()
 
 
 
 
 
-case_counts_dict = dict(Counter(map(lambda (f,y,c) : (c,y), cluster_data)))
-for c in dicts.keys():
-    for y in dicts[c]['case_counts'].keys():
-        if (c,y) in case_counts_dict:
-            dicts[c]['case_counts'][y] = case_counts_dict[(c,y)]
-print 'done'
-dicts[0]['case_counts'][1950]
-d1['0']['case_counts']['1950']
+#case_counts_dict = dict(Counter(map(lambda (f,y,c) : (c,y), cluster_data)))
+#for c in range(n_clusters):
+#    for y in dicts[c]['case_counts'].keys():
+#        if (c,y) in case_counts_dict:
+#            dicts[c]['case_counts'][y] = case_counts_dict[(c,y)]
+#            #print (c,y,dicts[0]['case_counts'][1950])
+#print 'done'
+
+#dicts[0]['case_counts'][1950]
+#d1['0']['case_counts']['1950']
 
 
 
@@ -71,10 +74,12 @@ d1['0']['case_counts']['1950']
 
 
 
-baseline_new = zip(itertools.product(years, range(n_clusters)), np.zeros(len(list(itertools.product(years, range(n_clusters))))))
-reduce(lambda old, (f,y,t) : [(tup,ct) for tup,ct in old if tup != (y,t)] + [((y,t), filter(lambda (tup,ct) : tup == (y,t), old)[0][1])], cluster_data, baseline_new)
+#baseline_new = zip(itertools.product(years, range(n_clusters)), np.zeros(len(list(itertools.product(years, range(n_clusters))))))
+#reduce(lambda old, (f,y,t) : [(tup,ct) for tup,ct in old if tup != (y,t)] + [((y,t), filter(lambda (tup,ct) : tup == (y,t), old)[0][1])], cluster_data, baseline_new)
 
 #case_counts_data = [((y,c), len(filter(lambda (f,y2,c2) : y2 == y and c2 == c, cluster_data))) for y,c in itertools.product(years, range(n_clusters))]
+
+
 
 #for f,y,t in cluster_data:
 for n, (f,y,t) in enumerate(cluster_data):
@@ -90,22 +95,15 @@ for n, (f,y,t) in enumerate(cluster_data):
     elif is_SC(f):
         dicts[t]['SC_counts'][y] += 1
     #print old, sum(dicts[24]['case_counts'].values())
-    if n % 100 == 0:
-        #print n, dicts[24]['case_counts'][2009]
-        sys.stdout.flush()
-print 'done iterating'
-sys.stdout.flush()
+    #if n % 100 == 0:
+    #    print n, dicts[24]['case_counts'][2009]
+    #    sys.stdout.flush()
+#print 'done iterating'
+#sys.stdout.flush()
 for c in range(n_clusters):
     for key in ['case_counts', 'dissent_counts', 'SC_counts', 'SC_dissent_counts']:
         dicts[c][key]['total'] = sum(dicts[c][key].values())
-print 'done totaling'
-sys.stdout.flush()
+#print 'done totaling'
+#sys.stdout.flush()
 
 json.dump(dicts, open(output_filename, 'w'))
-
-d1 = json.load(open('output.json', 'r'))
-d2 = json.load(open('output_new.json', 'r'))
-[d1[c] == d2[c] for c in d1.keys()]
-sum(d2['24']['case_counts'].values()) - 66982000#['total']
-d1['24']['case_counts']['2009']
-[(k, d1['24']['case_counts'][k], d2['24']['case_counts'][k]) for k in d1['24']['case_counts'].keys() if d1['24']['case_counts'][k] != d2['24']['case_counts'][k]]
