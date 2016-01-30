1f17c6bb2cd2b6177680a3ab6e45f79ef8e53430
madhuvijay95 <madhuvijay95@gmail.com>
2016-01-30 16:51:00 -0500

    changed sample.py to not write to stdout if an output file is specified

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 #######################################################################################################################
-# Inputs are (1) name of a file containing a list of cases and (2) decimal number representing the fraction of cases to
-# be sampled.
+# Inputs are (1) name of a file containing a list of cases; (2) decimal number representing the fraction of cases to
+# be sampled; and (3) name of an output file to which to write the subsample.
 # Creates a subsample of the cases (in which the proportion of cases showing up in the sample is specified by the second
 # argument), and writes the subsample case list to standard output.
 # Example: If case_list.txt contains a list of all the cases, then "python sample.py case_list.txt 0.15 > sample.txt"
@@ -13,10 +13,12 @@ import numpy as np
 threshold = float(sys.argv[2])
 with open(sys.argv[1], 'rb') as input_file:
     lst = [line.replace('\n','') for line in input_file]
-    sample_lst = np.random.choice(lst, size=threshold*len(lst), replace=False)
+
+sample_lst = np.random.choice(lst, size=threshold*len(lst), replace=False)
+if len(sys.argv) > 3:
+    with open(sys.argv[3], 'wb') as output_file:
+        for line in sample_lst:
+            output_file.write(line + '\n')
+else:
     for line in sample_lst:
         print line
-
-    #for line in input_file:
-    #    if np.random.random() < threshold:
-    #        print line.replace('\n','')
